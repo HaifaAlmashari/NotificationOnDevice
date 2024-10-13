e3e4cfd979fd810621707e0b696753c54648b70e
HaifaAlmashari <haifanasserr@gmail.com>
2024-10-13 23:32:37 +0300

    NotificationOnDevice

---
+++ app/src/main/java/com/example/notificationondevice/MainActivity.kt
@@ -0,0 +1,122 @@
+package com.example.notificationondevice
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.example.notificationondevice.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val CHANNEL_ID = "channel_id_example_01"
+    private val NOTIFICATION_ID = 101
+
+    lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+//        createNotificationChannel()
+
+//        binding.button.setOnClickListener{
+//            showNotification()
+//        }
+        showNotification()
+    }
+
+    private fun showNotification() {
+
+        val intent = Intent(applicationContext, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+
+        val pendingIntent = PendingIntent.getActivity(
+            applicationContext,
+            0, intent, 0
+        )
+
+
+        val notification = NotificationCompat.Builder(
+            applicationContext,
+            CHANNEL_ID
+        )
+            .setSmallIcon(R.drawable.ic_launcher_background)
+            .setContentTitle("Notification Title")
+            .setContentText("Notification Description")
+            .setPriority(NotificationCompat.PRIORITY_MAX)
+            .setAutoCancel(true)
+            .setContentIntent(pendingIntent)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+            val channelName = "Channel Name"
+            val channelDescription = "Channel Description"
+            val channelImportance = NotificationManager.IMPORTANCE_HIGH
+
+            val channel = NotificationChannel(CHANNEL_ID, channelName, channelImportance).apply {
+                description = channelDescription
+            }
+
+            val notificationManager = applicationContext.getSystemService(
+                Context.NOTIFICATION_SERVICE
+            ) as NotificationManager
+
+            notificationManager.createNotificationChannel(channel)
+        }
+
+
+        with(NotificationManagerCompat.from(applicationContext)) {
+            notify(NOTIFICATION_ID, notification.build())
+        }
+
+    }
+
+
+//    private fun createNotificationChannel(){
+//        // Create the NotificationChannel, but only on API 26+ because
+//        // the NotificationChannel class is new and not in the support library
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//            val name = "Notification Title"
+//            val descriptionText = "Notification Description"
+//            val importance = NotificationManager.IMPORTANCE_DEFAULT
+//            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+//                description = descriptionText
+//            }
+//            // Register the channel with the system
+//            val notificationManager: NotificationManager =
+//                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+//            notificationManager.createNotificationChannel(channel)
+//        }
+//    }
+//
+//    private fun sendNotification() {
+//        // Create an explicit intent for an Activity in your app
+//        val intent = Intent(this, MainActivity::class.java).apply {
+//            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+//        }
+//        val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
+//
+//        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+//            .setSmallIcon(R.drawable.ic_baseline_notifications_active_24)
+//            .setContentTitle("My notification")
+//            .setContentText("Hello World!")
+//            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+//            // Set the intent that will fire when the user taps the notification
+//            .setContentIntent(pendingIntent)
+//            .setAutoCancel(true)
+//
+//        with(NotificationManagerCompat.from(this)) {
+//            // notificationId is a unique int for each notification that you must define
+//            notify(notificationId, builder.build())
+//        }
+//    }
+}
